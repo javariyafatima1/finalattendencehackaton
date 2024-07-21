33a84e55daf6fdfb1b7edf6305b9c9ada0cb7004
javariyafatima1 <hoorfatima2729@gmail.com>
2024-07-21 11:58:30 +0500

    react bootsrap componenet use

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
   return (
     <div>
